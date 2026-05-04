c10f7df6b584e3316eb941c8d97101e5f1e9f60f
Timo Indlekofer <timo.indlekofer@web.de>
2026-05-04 15:18:07 +0200

    index.html: - add title - add header, main and footer - add images in header section - add hero images - add restaurant name and description
    standard.css:
    - add css classes for html, *, body, header, main and footer
    
    style.css:
    - add css classes for header section, hero container and images, restaurant name and description and for footer
    
    database.js
    - add JSON array with products
    
    - add font Palanquin
    - add fonts.css
    - add pictures for header logo, menu button, restaurant logo and picture

---
+++ scripts/database.js
@@ -0,0 +1,179 @@
+// ##### JavaScript database file #####
+
+let menu = [
+    {
+        "id": 101,
+        "name": "Mineralwasser",
+        "category": "Getränke",
+        "price": 2.50,
+        "description": "Stilles oder prickelndes Mineralwassser (0,5L Flasche)" 
+    },
+    {
+        "id": 102,
+        "name": "Cola / Spezi / Fanta",
+        "category": "Getränke",
+        "price": 3.20,
+        "description": "Cola, Spezi oder Fanta (0,33L Flasche)"   
+    },
+    {
+        "id": 103,
+        "name": "Hausgemachte Limonade",
+        "category": "Getränke",
+        "price": 4.50,
+        "description": "Köstliche, hausgemachte Limonade (0,5L Flasche)"
+    },
+    {
+        "id": 104,
+        "name": "Bier",
+        "category": "Getränke",
+        "price": 3.80,
+        "description": "Alkoholhaltiges oder alkholfreies, helles Bier (0,33L Flasche)" 
+    },
+    {
+        "id": 105,
+        "name": "Wein",
+        "category": "Getränke",
+        "price": 7.00,
+        "description": "Weiß- oder Rotwein (0,75L Flasche)" 
+    },
+    {
+        "id": 201,
+        "name": "Grüner Salat",
+        "category": "Vorspeise",
+        "price": 3.50,
+        "description": "Grüner Blattsalat in Essig-Öl-Dressing" 
+    },
+    {
+        "id": 202,
+        "name": "Gemischter Salat",
+        "category": "Vorspeise",
+        "price": 5.50,
+        "description": "Gemischter Blattsalat mit Karotten, Mais, Kraut und Kartoffelsalat in Essig-Öl-Dressing" 
+    },
+    {
+        "id": 203,
+        "name": "Frühlingsrollen",
+        "category": "Vorspeise",
+        "price": 4.50,
+        "description": "Vegetarisch gefüllt mit süß-sauer Sauce (3 Stück)" 
+    },
+    {
+        "id": 204,
+        "name": "Käse Sticks",
+        "category": "Vorspeise",
+        "price": 7.50,
+        "description": "Käse Sticks mit Chili-Dip (5 Stück)" 
+    },
+    {
+        "id": 205,
+        "name": "Mini-Quiches",
+        "category": "Vorspeise",
+        "price": 8.50,
+        "description": "Kleine, vegetarische Törtchen (3 Stück)" 
+    },
+    {
+        "id": 301,
+        "name": "Hamburger",
+        "category": "Hauptspeise",
+        "price": 12.50,
+        "description": "Burgerbans, 200g Rindfleisch, Salat, Gewürzgurkenscheiben, Tomatenscheiben mit Burgersauce" 
+    },
+    {
+        "id": 302,
+        "name": "Cheeseburger",
+        "category": "Hauptspeise",
+        "price": 14.50,
+        "description": "Burgerbans mit Sesamkörner, 200g Rindfleisch, 2 Scheiben Cheddar, Salat, Gewürzgurkenscheiben, Tomatenscheiben mit Burgersauce"
+    },
+    {
+        "id": 303,
+        "name": "Pizza",
+        "category": "Hauptspeise",
+        "price": 12.00,
+        "description": "Überraschungspizza nach Art des Hauses (30 cm)" 
+    },
+    {
+        "id": 304,
+        "name": "Schnitzel mit Pommes",
+        "category": "Hauptspeise",
+        "price": 18.50,
+        "description": "Rindfleischschnitzel mit Pommes, Reis oder Salat" 
+    },
+    {
+        "id": 305,
+        "name": "Vegetarisches Curry",
+        "category": "Hauptspeise",
+        "price": 12.50,
+        "description": "Saisonnales Gemüse in Curry-Sauce mit Reis" 
+    },
+    {
+        "id": 401,
+        "name": "Eisbecher",
+        "category": "Nachspeise",
+        "price": 7.00,
+        "description": "Vanilleeis mit Himbeeren (3 Kugeln)" 
+    },
+    {
+        "id": 402,
+        "name": "Joghurtbecher",
+        "category": "Nachspeise",
+        "price": 5.00,
+        "description": "Mit cremigen Waldhonig und Walnüssen" 
+    },
+    {
+        "id": 403,
+        "name": "Frische Waffeln",
+        "category": "Nachspeise",
+        "price": 4.80,
+        "description": "Vanilleeis mit Himbeeren (3 Kugeln)" 
+    },
+    {
+        "id": 404,
+        "name": "Kuchen",
+        "category": "Nachspeise",
+        "price": 5.30,
+        "description": "Frischer Erdbeer- oder Kirschkuchen (1 Stück)" 
+    },
+    {
+        "id": 405,
+        "name": "Gebackene Banane",
+        "category": "Nachspeise",
+        "price": 5.50,
+        "description": "In Teigmantel ausgebackene, mit Honig und Sesam verfeinerte Bananenstücke (7 Stücke)" 
+    },
+    {
+        "id": 501,
+        "name": "Portion Pommes",
+        "category": "Beilagen",
+        "price": 3.80,
+        "description": "Goldgelb und knusprig frittiert, mit Ketchup oder Mayo" 
+    },
+    {
+        "id": 502,
+        "name": "Kleine Kartoffeln",
+        "category": "Beilagen",
+        "price": 4.50,
+        "description": "Kleine Kartoffeln mit frischen Kräutern" 
+    },
+    {
+        "id": 503,
+        "name": "Portion Reis",
+        "category": "Beilagen",
+        "price": 3.50,
+        "description": "Locker gedämpfter Reis" 
+    },
+    {
+        "id": 504,
+        "name": "Pizzabrot",
+        "category": "Beilagen",
+        "price": 5.00,
+        "description": "Pizzabrot mit Olivenöl und Oregano (5 Stück)" 
+    },
+    {
+        "id": 505,
+        "name": "Gurkensalat",
+        "category": "Beilagen",
+        "price": 3.80,
+        "description": "Feine Gurkenscheiben in einem klassischen Dill-Essig-Dressing" 
+    }
+]
